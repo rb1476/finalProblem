e0e12882e7903fd076ff29caf1c3db86e48dfe31
Rafael Bacalso <rbacalso1024@gmail.com>
2019-03-20 14:32:12 +0000

    envelop toString axes

--- src/Envelop.java
+++ src/Envelop.java
@@ -35,7 +35,7 @@ public class Envelop {
 	public String toString() {
 		final int HEIGHT = maxYCoord();
 		final int WIDTH = maxXCoord();
-		char drawing[][] = new char[HEIGHT][WIDTH];
+		String drawing[][] = new String[HEIGHT][WIDTH];
 		
 		Coord currentPosition = new Coord(0, 0);
 		
@@ -67,25 +67,58 @@ public class Envelop {
 				// going right in graph
 				drawing[currentPosition.y][currentPosition.x] = "+";
 				
-				for (int i = currentPosition.x + 1; i < newPosition.x - 1, i++) {
-					drawing[currentPosition.y][i] = "-";
+				for (int i = currentPosition.x + 1; i < newPosition.x - 1; i++) {
+					drawing[currentPosition.y][i] = "--";
 				}
 				
-				drawing[currentPosition.y][currentPosition.x] = "+";
+				drawing[currentPosition.y][currentPosition.x] = "-+";
 			}
 		}
 		
 		StringBuilder lines = new StringBuilder();
+		int heightDigits = String.valueOf(HEIGHT).length();
+		
+		lines.append(" ".repeat(heightDigits));
+		lines.append(" .");
+		lines.append(" ".repeat(2 * WIDTH));
+		lines.append("\n");
+		//first line of graph
 		
 		for (int i = HEIGHT; i >= 0; i--) {
-			for (int j = 0; j <= WIDTH; i++) {
+			if (i == HEIGHT || i % 5 == 0) { //adds y axis labels (multiples of 5 and max)
+				lines.append(" ".repeat(heightDigits - String.valueOf(i).length())); //aligns digits in labels
+				lines.append(String.valueOf(i));
+				lines.append(" .");
+			}
+			
+			for (int j = 0; j <= WIDTH + 2; j++) { //adds whole line of graph
 				if (drawing[i][j] != null) {
 					lines.append(drawing[i][j]);
+				} else if (i == 0) { // draws x axis
+					if (j % 5 == 0) {
+						lines.append(" :");
+					} else {
+						lines.append(" .");
+					}
 				} else {
-					lines.append(" ")
+					lines.append("  ");
+				}
+			}
+			
+			lines.append("\n");
+		}
+		
+		for (int i = 0; i <= WIDTH; i++) { //adds x axis labels
+			if (i == WIDTH || i % 5 == 0) {
+				lines.append(String.valueOf(i));
+				if (i % 5 == 0) {
+					lines.append(" ".repeat(10 - String.valueOf(i).length()));
 				}
 			}
 		}
 		
+		String str = lines.toString();
+		
+		return str;
 	}
 }
